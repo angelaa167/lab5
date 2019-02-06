9a05875b763b7a3201f0b443bb44796b1595c27e
Angela <aml167@ucsd.edu>
2019-02-06 14:12:10 -0800

    finished

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,11 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	$('.friend-name').click(function (e) {
+		e.preventDefault();
+		$(this).find("h2").text(anagrammedName($(this).text()));
+	});
 })
 
 /*
